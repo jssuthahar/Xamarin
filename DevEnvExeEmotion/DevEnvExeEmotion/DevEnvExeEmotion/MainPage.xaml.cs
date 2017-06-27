efb9d3a704acfc9fda3958e81481efc33071dad6
Suthahar <jssuthahar@gmail.com>
2017-06-27 23:36:50 +0530

    MicroSoft AI
    MicroSoft AI

---
+++ DevEnvExeEmotion/DevEnvExeEmotion/DevEnvExeEmotion/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.ProjectOxford.Common.Contract;
+using Microsoft.ProjectOxford.Emotion;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DevEnvExeEmotion
+{
+    public partial class MainPage : ContentPage
+    {
+        EmotionServiceClient emotionClient;
+        MediaFile photo;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            emotion.IsVisible = false;
+            emotionClient = new EmotionServiceClient("7f495be5faf643adbeead444d5b79a13");
+        }
+        async void OnFeedBackClicked(object sender, EventArgs e)
+        {
+            //capture image
+            await CaptureImage();
+            ((Button)sender).IsEnabled = false;
+            // Recognize emotion
+            await Recognizeemotion();
+            ((Button)sender).IsEnabled = true;
+        }
+        public async Task CaptureImage()
+        {
+            await CrossMedia.Current.Initialize();
+            emotion.IsVisible = false;
+            // Take photo
+            if (CrossMedia.Current.IsCameraAvailable || CrossMedia.Current.IsTakePhotoSupported)
+            {
+                photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    SaveToAlbum = false
+                    
+                });
+
+            }
+            else
+            {
+                await DisplayAlert("No Camera", "Camera unavailable.", "OK");
+            }
+        }
+
+        public async Task Recognizeemotion()
+        {
+            try
+            {
+                if (photo != null)
+                {
+                    using (var photoStream = photo.GetStream())
+                    {
+
+                        Emotion[] emotionResult = await emotionClient.RecognizeAsync(photoStream);
+                        if (emotionResult.Any())
+                        {
+                            // Emotions detected are happiness, sadness, surprise, anger, fear, contempt, disgust, or neutral.
+                            emotionResultLabel.Text = emotionResult.FirstOrDefault().Scores.ToRankedList().FirstOrDefault().Key;
+                            emotion.IsVisible = true;
+                        }
+                        photo.Dispose();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ DevEnvExeFace/DevEnvExeFace/DevEnvExeFace/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.ProjectOxford.Common.Contract;
+using Microsoft.ProjectOxford.Emotion;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DevEnvExeFace
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void emotion_Clicked(object sender, EventArgs e)
+        {
+            var emotionClient = new EmotionServiceClient("ccd94bca7ccc4e0d9b45b200088990e9");
+            await CrossMedia.Current.Initialize();
+            if (CrossMedia.Current.IsCameraAvailable && CrossMedia.Current.IsTakePhotoSupported)
+            {
+                // Supply media options for saving our photo after it's taken.
+                var mediaOptions = new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    Directory = "Receipts",
+                    Name = $"{DateTime.UtcNow}.jpg"
+                };
+
+                // Take a photo of the business receipt.
+                var file = await CrossMedia.Current.TakePhotoAsync(mediaOptions);
+                 using (var photoStream = file.GetStream())
+                {
+                    Emotion[] emotionResult = await emotionClient.RecognizeAsync(photoStream);
+                    if (emotionResult.Any())
+                    {
+                        // Emotions detected are happiness, sadness, surprise, anger, fear, contempt, disgust, or neutral.
+                        lblrating.Text = emotionResult.FirstOrDefault().Scores.ToRankedList().FirstOrDefault().Key;
+                    }
+                    file.Dispose();
+                }
+            }
+
+            //var visionclient = new VisionServiceClient(visionKey);
+            //var result = await visionclient.DescribeAsync(file.GetStream());
+            //LblResult.Text = result.Description.Captions.First().Text;
+        }
+    }
+}
