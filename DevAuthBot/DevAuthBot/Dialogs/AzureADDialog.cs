5990b4f7e7ba9a3c42ccd33ede9518237d7a9d5e
Suthahar <jssuthahar@gmail.com>
2017-10-29 03:55:12 +0530

    Building Bot Application with Azure AD Login Authentication using AuthBot
    Building Bot Application with Azure AD Login Authentication using
    AuthBot

---
+++ DevAuthBot/DevAuthBot/Dialogs/AzureADDialog.cs
@@ -0,0 +1,60 @@
+﻿using AuthBot;
+using AuthBot.Dialogs;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Configuration;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevAuthBot.Dialogs
+{
+    [Serializable]
+    public class AzureADDialog : IDialog<string>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        /// <summary>
+        /// Login and Logout
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> item)
+        {
+            var message = await item;
+
+            //endpoint v1
+            if (string.IsNullOrEmpty(await context.GetAccessToken(ConfigurationManager.AppSettings["ActiveDirectory.ResourceId"])))
+            {
+                //Navigate to website for Login
+                await context.Forward(new AzureAuthDialog(ConfigurationManager.AppSettings["ActiveDirectory.ResourceId"]), this.ResumeAfterAuth, message, CancellationToken.None);
+            }
+            else
+            {
+                //Logout
+                await context.Logout();
+                context.Wait(MessageReceivedAsync);
+            }
+        }
+
+        /// <summary>
+        /// ResumeAfterAuth
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="result"></param>
+        /// <returns></returns>
+        private async Task ResumeAfterAuth(IDialogContext context, IAwaitable<string> result)
+        {
+            //AD resposnse message 
+            var message = await result;
+
+            await context.PostAsync(message);
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
+
---
+++ DevAuthBot/DevAuthBot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevAuthBot.WebApiApplication" Language="C#" %>
