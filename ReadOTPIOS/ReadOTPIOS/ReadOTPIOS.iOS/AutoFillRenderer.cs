81248a73dd4014c8b638097138244b9cdff16c40
Suthahar <jssuthahar@gmail.com>
2019-07-03 23:03:17 +0530

    Read OTP using Xamairn Forms
    Read OTP using Xamairn Forms

---
+++ ReadOTPIOS/ReadOTPIOS/ReadOTPIOS.iOS/AutoFillRenderer.cs
@@ -0,0 +1,23 @@
+﻿using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using ReadOTPIOS;
+using ReadOTPIOS.iOS;
+[assembly: ExportRenderer(typeof(AutoFillControl), typeof(AutoFillRenderer))]
+namespace ReadOTPIOS.iOS
+{
+   
+    public class AutoFillRenderer: EntryRenderer
+    {
+       
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                Control.TextContentType = UITextContentType.OneTimeCode;
+            }
+        }
+    }
+}
---
+++ ReadOTPIOS/ReadOTPIOS/ReadOTPIOS/AutoFillControl.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ReadOTPIOS
+{
+    public class AutoFillControl : Entry
+    {
+        public AutoFillControl()
+        {
+        }
+    }
+}
