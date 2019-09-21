97df55234ad527bfdd42c0aa1762fcbf13796204
Suthahar <jssuthahar@gmail.com>
2019-09-21 20:24:12 +0530

    Autofill OTP
    Autofill OTP

--- ReadOTPIOS/ReadOTPIOS/ReadOTPIOS.iOS/AutoFillRenderer.cs
+++ ReadOTPIOS/ReadOTPIOS/ReadOTPIOS.iOS/AutoFillRenderer.cs
@@ -1,15 +1,13 @@
-﻿using UIKit;
-using Xamarin.Forms;
-using Xamarin.Forms.Platform.iOS;
-using ReadOTPIOS;
+﻿using ReadOTPIOS;
 using ReadOTPIOS.iOS;
+using UIKit;
+
+
 [assembly: ExportRenderer(typeof(AutoFillControl), typeof(AutoFillRenderer))]
 namespace ReadOTPIOS.iOS
 {
-   
     public class AutoFillRenderer: EntryRenderer
     {
-       
         protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
         {
             base.OnElementChanged(e);
