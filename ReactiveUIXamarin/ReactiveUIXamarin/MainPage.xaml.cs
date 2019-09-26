89127fa31e0daef6c8391ef7a9bd3ca2db4a83c7
Suthahar <jssuthahar@gmail.com>
2019-09-26 23:51:19 +0530

    Reactive UI Xamarin Forms
    Reactive UI Xamarin Forms

---
+++ ReactiveUIXamarin/ReactiveUIXamarin/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ReactiveUIXamarin.ViewModel;
+using ReactiveUI.XamForms;
+using System.Reactive.Disposables;
+
+namespace ReactiveUIXamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+
+    public partial class MainPage : ReactiveContentPage<MainPageViewModel>
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            ViewModel = new MainPageViewModel();
+
+            // Setup the bindings.
+            // Note: We have to use WhenActivated here, since we need to dispose the
+            // bindings on XAML-based platforms, or else the bindings leak memory.
+            this.WhenActivated(disposable =>
+            {
+                this.Bind(ViewModel, x => x.UserName, x => x.Username.Text)
+                    .DisposeWith(disposable);
+                this.Bind(ViewModel, x => x.Password, x => x.Password.Text)
+                    .DisposeWith(disposable);
+                this.Bind(ViewModel, x => x.Address, x => x.Address.Text)
+                   .DisposeWith(disposable);
+                this.Bind(ViewModel, x => x.Phone, x => x.Phone.Text)
+                   .DisposeWith(disposable);
+                this.BindCommand(ViewModel, x => x.RegisterCommand, x => x.Register)
+                  
+                    .DisposeWith(disposable);
+                this.Bind(ViewModel, x => x.Result, x => x.Result.Text)
+                   .DisposeWith(disposable);
+            });
+        }
+    }
+}
---
+++ ReactiveUIXamarin/ReactiveUIXamarin/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using ReactiveUI;
+using System.Reactive;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+
+namespace ReactiveUIXamarin.ViewModel
+{
+    public class MainPageViewModel: ReactiveObject
+    {
+        private string _result;
+        public string Result
+        {
+            get => _result;
+            set => this.RaiseAndSetIfChanged(ref _result, value);
+        }
+        private string _username;
+        public string UserName
+        {
+            get => _username;
+            set => this.RaiseAndSetIfChanged(ref _username, value);
+        }
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => this.RaiseAndSetIfChanged(ref _password, value);
+        }
+        private string _address;
+        public string Address
+        {
+            get => _address;
+            set => this.RaiseAndSetIfChanged(ref _address, value);
+        }
+        private string _phone;
+        public string Phone
+        {
+            get => _phone;
+            set => this.RaiseAndSetIfChanged(ref _phone, value);
+        }
+        public ReactiveCommand<Unit, Unit> RegisterCommand { get; }
+
+        public MainPageViewModel()
+        {
+            RegisterCommand = ReactiveCommand
+                .CreateFromObservable(ExecuteRegisterCommand);
+        }
+
+        private IObservable<Unit> ExecuteRegisterCommand()
+        {
+            Result = "Hello" + UserName + " Registration Success";
+            return Observable.Return(Unit.Default);
+        }
+    }
+  
+}
+
